badfc5b45a6994e4dd4365c1732506aa804a4487
gigiali <ali.je@husky.neu.edu>
2021-03-26 03:33:33 -0400

    scatterplot shows up; no lines

--- brushinglinkingfinal.js
+++ brushinglinkingfinal.js
@@ -31,30 +31,33 @@ var svg3 = d3
 var color = d3
   .scaleOrdinal()
   .domain(["Red", "Orange", "Green", "Blue"])
-  .range(["#e62525", "#eb9307", "#067318", "#2936ab"]);
+  .range(["#808791", "#eb9307", "#067318", "#808791"]);
 
 //Load Data
 d3.csv("MBTA_Line_and_Stop.csv").then((data) => {
-  // Scatterplot 1
   {
 
-    let scData = [
-      { time_period_name: "AM_PEAK", avg: 50 },
-      { time_period_name: "EVENING", avg: 30 },
-      { time_period_name: "MIDDAY_SCHOOL", avg: 60 },
-    ];
-
+      let lineData = [
+        {time_period_name: "VERY_EARLY_MORNING", avg: 223},
+        {time_period_name: "EARLY_AM", avg: 840},
+        {time_period_name: "AM_PEAK	", avg: 3114},
+        {time_period_name: "MIDDAY_BASE", avg: 2766},
+        {time_period_name: "MIDDAY_SCHOOL", avg: 2022},
+        {time_period_name: "PM_PEAK", avg: 3688},
+        {time_period_name: "EVENING", avg: 1983},
+        {time_period_name: "LATE_EVENING", avg: 583},
+        {time_period_name: "NIGHT", avg: 84},
+        {time_period_name: "OFF_PEAK", avg: 6517}];
+    // scatterplot(?) 1
     var xKey1 = "time_period_name";
     var yKey1 = "avg";
 
     //Add X axis
     var x1 = d3
-      .scaleLinear()
-      .domain(scData.map(function (d) {
+      .scaleBand()
+      .domain(lineData.map(function (d) {
         return d["time_period_name"];
       }))
-      // .domain(scData.map(function(d) { return d.time_period_name; }))
-      // .domain(d3.extent(scData.map((val) => val[xKey1])))
       .range([0, width]);
     svg1
       .append("g")
@@ -73,8 +76,8 @@ d3.csv("MBTA_Line_and_Stop.csv").then((data) => {
     //Add Y axis
     var y1 = d3
       .scaleLinear()
-      .domain(d3.extent(scData.map((val) => val[yKey1])))
-      .range([height, 0]);
+      .domain(d3.extent(lineData.map((val) => val[yKey1])))
+      .range([height,0]);
     svg1
       .append("g")
       .call(d3.axisLeft(y1))
@@ -92,7 +95,7 @@ d3.csv("MBTA_Line_and_Stop.csv").then((data) => {
     var myCircle1 = svg1
       .append("g")
       .selectAll("circle")
-      .data(scData)
+      .data(lineData)
       .enter()
       .append("circle")
       .attr("id", (d) => d.id)
@@ -121,6 +124,92 @@ d3.csv("MBTA_Line_and_Stop.csv").then((data) => {
     //Adding brush to the svg
     svg1.call(brush1);
   }
+  // Scatterplot 1
+  // {
+
+  //   let scData = [
+  //     { time_period_name: "AM_PEAK", avg: 50 },
+  //     { time_period_name: "EVENING", avg: 30 },
+  //     { time_period_name: "MIDDAY_SCHOOL", avg: 60 },
+  //   ];
+
+  //   var xKey1 = "time_period_name";
+  //   var yKey1 = "avg";
+
+  //   //Add X axis
+  //   var x1 = d3
+  //     .scaleLinear()
+  //     .domain(scData.map(function (d) {
+  //       return d["time_period_name"];
+  //     }))
+  //     // .domain(scData.map(function(d) { return d.time_period_name; }))
+  //     // .domain(d3.extent(scData.map((val) => val[xKey1])))
+  //     .range([0, width]);
+  //   svg1
+  //     .append("g")
+  //     .attr("transform", "translate(0," + height + ")")
+  //     .call(d3.axisBottom(x1))
+  //     .call((g) =>
+  //       g
+  //         .append("text")
+  //         .attr("x", width)
+  //         .attr("y", margin.bottom - 4)
+  //         .attr("fill", "currentColor")
+  //         .attr("text-anchor", "end")
+  //         .text(xKey1)
+  //     );
+
+  //   //Add Y axis
+  //   var y1 = d3
+  //     .scaleLinear()
+  //     .domain(d3.extent(scData.map((val) => val[yKey1])))
+  //     .range([height, 0]);
+  //   svg1
+  //     .append("g")
+  //     .call(d3.axisLeft(y1))
+  //     .call((g) =>
+  //       g
+  //         .append("text")
+  //         .attr("x", -margin.left)
+  //         .attr("y", 10)
+  //         .attr("fill", "currentColor")
+  //         .attr("text-anchor", "start")
+  //         .text(yKey1)
+  //     );
+
+  //   // Add dots
+  //   var myCircle1 = svg1
+  //     .append("g")
+  //     .selectAll("circle")
+  //     .data(scData)
+  //     .enter()
+  //     .append("circle")
+  //     .attr("id", (d) => d.id)
+  //     .attr("cx", function (d) {
+  //       return x1(d[xKey1]);
+  //     })
+  //     .attr("cy", function (d) {
+  //       return y1(d[yKey1]);
+  //     })
+  //     .attr("r", 8)
+  //     .style("fill", function (d) {
+  //       return color(d.Species);
+  //     })
+  //     .style("opacity", 0.5);
+
+  //   //Define a brush
+  //   var brush1 = d3
+  //     .brush()
+  //     .extent([
+  //       [0, 0],
+  //       [width, height],
+  //     ])
+  //     .on("start", clear)
+  //     .on("brush", updateChart1);
+
+  //   //Adding brush to the svg
+  //   svg1.call(brush1);
+  // }
 
   // Scatterplot 2
   {
@@ -305,3 +394,59 @@ d3.csv("MBTA_Line_and_Stop.csv").then((data) => {
     return x0 <= cx && cx <= x1 && y0 <= cy && cy <= y1; // This return TRUE or FALSE depending on if the points is in the selected area
   }
 });
+
+
+// var margin = {top: 10, right: 30, bottom: 30, left: 60},
+//     width = 460 - margin.left - margin.right,
+//     height = 400 - margin.top - margin.bottom;
+// append the svg object to the body of the page
+var svg = d3.select("#dataviz_brushScatter")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")");
+//Read the data
+d3.csv("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/connectedscatter.csv").then((data) => {
+  // When reading the csv, I must format variables:
+  // function(d){
+  //   return { date : d3.timeParse("%Y-%m-%d")(d.date), value : d.value }
+  // },
+  // Now I can use this dataset:
+
+    // Add X axis --> it is a date format
+    var x = d3.scaleTime()
+      .domain(d3.extent(data, function(d) { return d.date; }))
+      .range([ 0, width ]);
+    svg.append("g")
+      .attr("transform", "translate(0," + height + ")")
+      .call(d3.axisBottom(x));
+    // Add Y axis
+    var y = d3.scaleLinear()
+      .domain( [8000, 9200])
+      .range([ height, 0 ]);
+    svg.append("g")
+      .call(d3.axisLeft(y));
+    // Add the line
+    svg.append("path")
+      .datum(data)
+      .attr("fill", "none")
+      .attr("stroke", "#69b3a2")
+      .attr("stroke-width", 1.5)
+      .attr("d", d3.line()
+        .x(function(d) { return x(d.date) })
+        .y(function(d) { return y(d.value) })
+        )
+    // Add the points
+    svg
+      .append("g")
+      .selectAll("dot")
+      .data(data)
+      .enter()
+      .append("circle")
+        .attr("cx", function(d) { return x(d.date) } )
+        .attr("cy", function(d) { return y(d.value) } )
+        .attr("r", 5)
+        .attr("fill", "#69b3a2")
+})
